43520bed965ae10017a5e799c4859c390e5c62b4
izumin5210 <m@izum.in>
2022-03-26 16:40:56 +0900

    Download assets

--- fetcher.go
+++ fetcher.go
@@ -1,11 +1,16 @@
 package fetchwebpage
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"net/url"
 	"os"
+	"path/filepath"
+	"sync"
 
+	"github.com/PuerkitoBio/goquery"
+	"github.com/hashicorp/go-multierror"
 	"github.com/spf13/afero"
 )
 
@@ -33,13 +38,86 @@ func (f *fetcherImpl) Fetch(ctx context.Context, givenURL string) error {
 
 	filename := fmt.Sprintf("%s.html", parsedURL.Host)
 
-	file, err := f.fs.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.FileMode(0644))
+	var buf bytes.Buffer
+	err = f.downloader.Download(ctx, givenURL, &buf)
 	if err != nil {
-		return fmt.Errorf("failed to open file: %w", err)
+		return fmt.Errorf("failed to download file: %w", err)
 	}
-	defer file.Close()
 
-	err = f.downloader.Download(ctx, givenURL, file)
+	assetDirname := fmt.Sprintf("%s_assets", filename)
+
+	downloadAsset := func(path *url.URL) error {
+		assetURL := parsedURL.ResolveReference(path)
+
+		filename := filepath.Join(assetDirname, assetURL.RequestURI())
+		dir := filepath.Dir(filename)
+
+		err = f.fs.MkdirAll(dir, os.FileMode(0755))
+		if err != nil {
+			return fmt.Errorf("failed to create assets directory: %w", err)
+		}
+
+		file, err := f.fs.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.FileMode(0644))
+		if err != nil {
+			return fmt.Errorf("failed to open file: %w", err)
+		}
+
+		err = f.downloader.Download(ctx, assetURL.String(), file)
+		if err != nil {
+			return fmt.Errorf("failed to download file: %w", err)
+		}
+
+		return nil
+	}
+
+	var wg sync.WaitGroup
+
+	errCh := make(chan error)
+	var combinedErr error
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for err := range errCh {
+			combinedErr = multierror.Append(combinedErr, err)
+		}
+	}()
+
+	assetPathCh := make(chan *url.URL)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for path := range assetPathCh {
+			err := downloadAsset(path)
+			if err != nil {
+				errCh <- fmt.Errorf("failed to download asset: %w", err)
+			}
+		}
+	}()
+
+	doc, err := goquery.NewDocumentFromReader(&buf)
+	if err != nil {
+		return fmt.Errorf("failed to parse html file: %w", err)
+	}
+	doc.Find("img").Each(func(i int, s *goquery.Selection) {
+		if src, ok := s.Attr("src"); ok {
+			if u, err := url.Parse(src); err == nil && !u.IsAbs() {
+				assetPathCh <- u
+				newPath := filepath.Join(assetDirname, u.RequestURI())
+				s.SetAttr("src", newPath)
+			}
+		}
+	})
+
+	close(assetPathCh)
+	close(errCh)
+	wg.Wait()
+
+	html, err := doc.Html()
+	if err != nil {
+		return fmt.Errorf("failed to generate manipulated html: %w", err)
+	}
+
+	err = afero.WriteFile(f.fs, filename, []byte(html), os.FileMode(0664))
 	if err != nil {
 		return fmt.Errorf("failed to write file: %w", err)
 	}
--- go.mod
+++ go.mod
@@ -3,14 +3,17 @@ module github.com/izumin5210-sandbox/fetch-webpage
 go 1.18
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/hashicorp/go-multierror v1.1.1
 	github.com/spf13/afero v1.8.2
 	github.com/spf13/cobra v1.4.0
 )
 
 require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/text v0.3.4 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
